cc35e1db5f1e0fd8e78c9bf4568df4b6a9254fc3
Mattia Falco <mattiafalco87@gmail.com>
2022-11-24 12:43:36 +0100

    update2

--- code/esercizio1/src/accademy/it/corso/Main.java
+++ code/esercizio1/src/accademy/it/corso/Main.java
@@ -2,6 +2,7 @@ package accademy.it.corso;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!\n");
+        System.out.println("Mattia Falco");
     }
 }
