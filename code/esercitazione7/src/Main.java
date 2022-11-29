bda4d2162d2a9a1aa89022169da73fc3e0502a30
Mattia Falco <mattiafalco87@gmail.com>
2022-11-29 10:18:36 +0100

    try/catch

--- code/esercitazione7/src/Main.java
+++ code/esercitazione7/src/Main.java
@@ -1,7 +1,23 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int[] array = {45,34,24,78,65,43,21,67,89,11};
+        int[] array = new int[10];
         int temp;
+
+        Scanner scn = new Scanner(System.in);
+
+        for(int i = 0; i < 10; i++){
+            try{
+                System.out.println("Inserisci il "+(i+1)+" numero : ");
+                array[i] = scn.nextInt();
+            }catch(InputMismatchException e){
+                System.out.println("Error : " + e);
+            }catch (Exception e){
+                System.out.println("Error : " + e);
+            }
+        }
 
         for(int i = 0; i < (array.length - 1); i++){
             for(int j = 1; j<(array.length - i); j++){
